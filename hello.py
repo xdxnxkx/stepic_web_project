bd678ab236527a4862c98d3e3a6935709204ce83
xdxnxkx <xdxnxkx@gmail.com>
2016-03-17 04:27:48 +0300

    Обновил код приложения.

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
- from cgi import parse_qs
- def app(environ, start_response):
-      start_response("200 OK", [("Content-Type", "text/plain")])
-      qs = parse_qs(environ['QUERY_STRING'])
-      return ['%s=%s<br>' % (k,qs[k][0]) for k in qs]
+from urlparse import parse_qs
+
+
+def app(environ, start_response):
+    start_response("200 OK", [("Content-Type", "text/plain")])
+    qs = parse_qs(environ['QUERY_STRING'])
+    return ['%s=%s<br>' % (k, qs[k][0]) for k in qs]
