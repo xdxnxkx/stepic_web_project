d2e7854551ebb665bdd19766e160e451d53fa958
xdxnxkx <xdxnxkx@gmail.com>
2016-03-17 05:21:04 +0300

    Внес правки.

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
-from urlparse import parse_qs
-
-
 def app(environ, start_response):
     start_response("200 OK", [("Content-Type", "text/plain")])
-    qs = parse_qs(environ['QUERY_STRING'])
-    return ['%s=%s<br>' % (k, qs[k][0]) for k in qs]
+    qs = str(environ['QUERY_STRING'])
+    nqs = ''
+    for x in qs:
+        if x == '&':
+            nqs += '\n'
+        nqs += x
+    return [nqs]
