d20393e41f60ff639034edaac164e0a37567305c
xdxnxkx <xdxnxkx@gmail.com>
2016-03-22 03:45:36 +0300

    Очередные изменения в разрабатываемый сайт_2.

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn_1.conf /etc/gunicorn.d/test_1
+sudo ln -sf /home/box/web/etc/gunicorn_2.conf /etc/gunicorn.d/test_2
 sudo /etc/init.d/gunicorn restart
