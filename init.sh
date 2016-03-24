0ff5fa6212d489b4534126e2fb9bfe1eb88a7ecf
xdxnxkx <xdxnxkx@gmail.com>
2016-03-24 04:06:25 +0300

    Разработка баз данных_1.

--- init.sh
+++ init.sh
@@ -5,3 +5,7 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn_help.py /etc/gunicorn.d/test_1
 sudo ln -sf /home/box/web/etc/gunicorn_wsgi.py /etc/gunicorn.d/test_2
 sudo /etc/init.d/gunicorn restart
+sudo service mysqld start
+mysql -u root -e "CREATE DATABASE db_stepic;"
+mysql -u root -e "CREATE USER 'xdxnxkx'@'localhost' IDENTIFIED BY 'dnk-00003' PASSWORD EXPIRE;"
+
