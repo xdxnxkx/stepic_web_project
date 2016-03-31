d6a64480e306c286a603cfd221dd4684157a4e98
xdxnxkx <xdxnxkx@gmail.com>
2016-03-31 23:29:56 +0300

    Разработка баз данных_3.

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Question(models.Model):
+
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author =
+    likes =
+
+class Answer(models.Model):
+
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question =
+    author =
