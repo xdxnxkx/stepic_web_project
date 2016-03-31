fff5c8066ed373f165d5651f3ac872313d894c7c
xdxnxkx <xdxnxkx@gmail.com>
2016-04-01 00:51:03 +0300

    Разработка баз данных_4.

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,19 +3,17 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-
 class Question(models.Model):
-
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField()
     rating = models.IntegerField()
-    author =
-    likes =
+    author = models.OneToOneField(User)
+    likes = models.ForeignKey(User)
+
 
 class Answer(models.Model):
-
     text = models.TextField()
     added_at = models.DateTimeField()
-    question =
-    author =
+    question = models.OneToOneField(Question)
+    author = models.OneToOneField(User)
