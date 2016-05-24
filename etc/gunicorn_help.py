a7da50637ae6508fc565c9a645b99c88e6108fb6
xdxnxkx <xdxnxkx@gmail.com>
2016-05-24 08:01:13 +0300

    Развертка серверов у себя. Создание Init_2.

--- etc/gunicorn_help.py
+++ etc/gunicorn_help.py
@@ -1,6 +1,10 @@
+import os
+
+dirct = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 CONFIG = {
     'mode': 'wsgi',
-    'working_dir': '/home/box/web',
+    'working_dir': dirct,
     # 'python': '/usr/bin/python',
     'args': (
         '--bind=0.0.0.0:8080',
--- init_2.sh
+++ init_2.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn_help.py /etc/gunicorn.d/test_1
+sudo ln -sf /home/daniil/Documents/py_coding/stepic_project/etc/nginx.conf /etc/nginx/conf.t/test.conf
+sudo nginx -s reload
+cd
 sudo ln -sf /home/box/web/etc/gunicorn_wsgi.py /etc/gunicorn.d/test_2
 sudo /etc/init.d/gunicorn restart
 sudo service mysqld restart
 mysql -u root -e "CREATE DATABASE db_stepic;"
 mysql -u root -e "CREATE USER 'xdxnxkx'@'localhost' IDENTIFIED BY 'Lyr-00003' PASSWORD EXPIRE;"
 mysql -u root -e "GRANT ALL ON db_stepic.* TO 'xdxnxkx'@'localhost';"
-
