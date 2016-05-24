313210b92cb028a9dcdbb16673c8d76faa97fd0c
xdxnxkx <xdxnxkx@gmail.com>
2016-05-24 08:28:54 +0300

    Правка hello приложения.

--- hello.py
+++ hello.py
@@ -2,8 +2,9 @@ def app(environ, start_response):
     start_response("200 OK", [("Content-Type", "text/plain")])
     qs = str(environ['QUERY_STRING'])
     nqs = ''
-    for x in qs:
-        if x == '&':
-            nqs += '\n'
-        nqs += x
-    return [nqs]
+    nqs = nqs.replace("&", '\n')
+    # for x in qs:
+    #     if x == '&':
+    #         nqs += '\n'
+    #     nqs += x
+    return bytes([nqs])
